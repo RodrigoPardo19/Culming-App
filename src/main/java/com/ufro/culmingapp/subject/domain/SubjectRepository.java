c40734c8e9c3efc0867568d4f84235c729ad72bd
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2021-11-29 13:38:41 -0300

    Endpoint to get subjects taken by a student was added

--- src/main/java/com/ufro/culmingapp/subject/domain/SubjectRepository.java
+++ src/main/java/com/ufro/culmingapp/subject/domain/SubjectRepository.java
@@ -1,20 +1,24 @@
 package com.ufro.culmingapp.subject.domain;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.ufro.culmingapp.subject.application.DTOs.SubjectDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
 
     @Query(value = "SELECT * FROM SUBJECTS WHERE ID IN" +
-           "(SELECT SUBJECT_ID FROM TEACHERS_SUBJECTS WHERE TEACHER_ID = :teacherId AND SUBJECT_ID IN " +
-           "(SELECT SUBJECT_ID FROM STUDENTS_SUBJECTS WHERE STUDENT_ID IN" +
-           "(SELECT ID FROM STUDENTS WHERE ID IN " +
-           "(SELECT STUDENT_ID FROM STUDENTS_COURSES WHERE COURSE_ID= :courseId))))", nativeQuery = true)
-    public Optional<List<Subject>> findSubjectsTaughtByATeacherInACourse(@Param("teacherId") Long teacherId,
-            @Param("courseId") Long courseId);
+            "(SELECT SUBJECT_ID FROM TEACHERS_SUBJECTS WHERE TEACHER_ID = :teacherId AND SUBJECT_ID IN " +
+            "(SELECT SUBJECT_ID FROM STUDENTS_SUBJECTS WHERE STUDENT_ID IN" +
+            "(SELECT ID FROM STUDENTS WHERE ID IN " +
+            "(SELECT STUDENT_ID FROM STUDENTS_COURSES WHERE COURSE_ID= :courseId))))", nativeQuery = true)
+    Optional<List<Subject>> findSubjectsTaughtByATeacherInACourse(@Param("teacherId") Long teacherId,
+                                                                  @Param("courseId") Long courseId);
 
+    @Query("SELECT new com.ufro.culmingapp.subject.application.DTOs.SubjectDTO(s.id, s.name.name) FROM Subject s JOIN " +
+            " s.students sts WHERE sts.year = 2021 AND sts.student.id = :studentId")
+    Optional<List<SubjectDTO>> fetchSubjectsTakenByAStudent(@Param("studentId") Long studentId);
 }
---
+++ src/main/java/com/ufro/culmingapp/subject/infrastructure/SubjectGetController.java
@@ -0,0 +1,28 @@
+package com.ufro.culmingapp.subject.infrastructure;
+
+import com.ufro.culmingapp.subject.application.DTOs.SubjectDTO;
+import com.ufro.culmingapp.subject.application.SubjectFinderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SubjectGetController {
+
+    @Autowired
+    private SubjectFinderService finder;
+
+    @GetMapping("/students/{studentId}/subjects")
+    public ResponseEntity<?> getSubjectsTakenByAStudent(@PathVariable Long studentId) {
+
+        List<SubjectDTO> subjects = finder.getSubjectsTakenByAStudent(studentId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(subjects);
+    }
+}
+
